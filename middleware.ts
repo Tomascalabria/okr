12eba2bd8eac283993ad543a1fa2629e7f21e920
Tomas Gabriel Calabria <81932784+Tomascalabria@users.noreply.github.com>
2025-01-13 14:40:08 -0300

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,6 @@
-import { createMiddlewareClient } from "@supabase/auth-helpers-nextjs";
-import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
+import { createMiddlewareClient } from '@supabase/auth-helpers-nextjs';
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
 
 export async function middleware(req: NextRequest) {
   const res = NextResponse.next();
@@ -12,21 +12,25 @@ export async function middleware(req: NextRequest) {
 
   console.log("Session:", session); // Verifica si la sesión está disponible
 
-  const isProtectedRoute = ["/hero", "/group"].some((route) =>
+  // Rutas protegidas que requieren autenticación
+  const protectedRoutes = ['/hero', '/group'];
+  const isProtectedRoute = protectedRoutes.some(route =>
     req.nextUrl.pathname.startsWith(route)
   );
-  const isAuthPage = req.nextUrl.pathname.startsWith("/auth");
-  const isLoginPage = req.nextUrl.pathname === "/auth/login";
 
-  if (!session) {
-    if (isProtectedRoute) {
-      return NextResponse.redirect(new URL("/auth/login", req.url));
-    }
-  } else {
-    if (isAuthPage && !isLoginPage) {
-      return NextResponse.redirect(new URL("/hero", req.url));
-    }
+  const isAuthPage = req.nextUrl.pathname.startsWith('/auth');
+  const isLoginPage = req.nextUrl.pathname === '/auth/login';
+
+  // Redirigir a la página de autenticación si no hay sesión y se intenta acceder a una ruta protegida
+  if (!session && isProtectedRoute && !isAuthPage) {
+    return NextResponse.redirect(new URL('/auth/login', req.url));
   }
 
+  // Redirigir a la página de dashboard si ya hay una sesión y se intenta acceder a una página de autenticación
+  if (session && isAuthPage && !isLoginPage) {
+    return NextResponse.redirect(new URL('/hero', req.url));
+  }
+
+  // Si no se cumple ninguna de las condiciones anteriores, se devuelve la respuesta original
   return res;
 }
