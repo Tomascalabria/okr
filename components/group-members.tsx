8b8233e6b570db9d2f80bf480821b707f52716f5
Tomas Gabriel Calabria <81932784+Tomascalabria@users.noreply.github.com>
2025-01-16 15:56:37 -0300

    Update group-members.tsx

--- components/group-members.tsx
+++ components/group-members.tsx
@@ -61,7 +61,7 @@ useEffect(() => {
         // Mapea los miembros con sus perfiles, asegurándote de que 'profiles' exista
         const formattedMembersData = membersData.map((member) => {
           // Verifica si 'profiles' existe y asigna un valor predeterminado si es necesario
-          const profileData = member.profiles ? member.profiles : { name: "Sin Nombre", avatar_url: null };
+          const profileData = member.profiles
 
           return {
             user_id: member.user_id,
